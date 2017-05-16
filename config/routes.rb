d0caefd7a7a97498def502c63bef6be5841aaaeb
macksimiljan <macksimiljan@users.noreply.github.com>
2017-05-16 15:21:43 +0200

    implemented simple view for websites.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'websites#index'
+
+  resources :websites, only: [:show]
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 end
