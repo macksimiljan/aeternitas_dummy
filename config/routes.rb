4a1577ed3aec6a746528e0fc8b931b2f88452a1b
macksimiljan <macksimiljan@users.noreply.github.com>
2017-05-18 08:54:33 +0200

    created dashboard for job management and monitoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'websites#index'
+  root 'dashboard#index'
 
+  resources :dashboard, only: [:index]
   resources :websites
 
   require 'sidekiq/web'
